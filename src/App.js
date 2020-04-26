efff9ba03006980a290947d493fd387e4c9cb888
Nicholas <ygannicholas@gmail.com>
2020-04-26 04:14:59 -0400

    Added form validation
    I added validation to the form and provided the feedback on the front end through a red box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Form} from './form/form'
 import {TopAppBar, TopAppBarRow, TopAppBarSection, TopAppBarTitle, TopAppBarFixedAdjust} from '@rmwc/top-app-bar'
 import '@rmwc/top-app-bar/styles';
 
 
 function App() {
-  return (
-      <div>
-          <TopAppBar>
-              <TopAppBarRow>
-                  <TopAppBarSection>
-                      <TopAppBarTitle>MedSpeech</TopAppBarTitle>
-                  </TopAppBarSection>
-              </TopAppBarRow>
-          </TopAppBar>
-          <TopAppBarFixedAdjust />
-          <Form/>
-      </div>
-  );
+    let [err, setErr] = useState("")
+    let errBox = document.getElementById("errorString")
+    let formTopBorder = 100 // px
+    if (err !== "") formTopBorder = "1000"
+
+
+    return (
+        <div>
+            <TopAppBar>
+                <TopAppBarRow>
+                    <TopAppBarSection>
+                        <TopAppBarTitle>MedSpeech</TopAppBarTitle>
+                    </TopAppBarSection>
+                </TopAppBarRow>
+                {
+                    err !== "" ? <div style={{
+                            "width": "100%",
+                            "background-color": "red",
+                            "padding": "10px",
+                            "text-align": "center"
+                        }} id="errorString">
+                            {err}
+                        </div>
+                        : ""
+                }
+            </TopAppBar>
+            <TopAppBarFixedAdjust/>
+
+            <Form setError={(error) => {setErr(error)}} topPadding={formTopBorder}/>
+        </div>
+    );
 }
 
 export default App;
